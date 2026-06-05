f85d7f46e7d90d141257e016c9eee599a43701e2
DINESHWARAN.K <dineshwaran236@gmail.com>
2026-06-06 01:04:02 +0530

    first deploy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // <-- ADD THIS BLOCK HERE
+  },
 };
 
 export default nextConfig;
